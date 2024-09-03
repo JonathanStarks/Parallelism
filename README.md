f59349cc924d2f2ad589a5c46d6025ae0f56b4c9
JonathanStarks <jonstarks@byui.edu>
2024-09-03 12:38:05 -0700

    Got a working program

--- README.md
+++ README.md
@@ -3,9 +3,17 @@ A repository to demonstrate that I have learned and can apply (at least somewhat
 
 ### Description
 
-This Code Uses an API from the CDC that contains information on foodborne and waterborne diseases. There are two primary functions, the first one will run the data, getting the year, month, state, mode of transmission, the name of the disease, where it was caught, and the number of deaths, and then will display the information without using threads. The second function will do the same, except it will use threads, senaphores, and locks to speed up the process.
+This Code Uses an API from the CDC that contains information on foodborne and waterborne diseases. There are two primary functions, the first one will go through the data looking at the year, month, state, mode of transmission, name of the disease that caused the outbreak, environment where patient 0 was infected, and the number of deaths from the outbreak.
 
 ### Helpful Recources
 * [ChatGPT](chatgpt.com)
 * [stackoverflow](stackoverflow.com)
 * [CDC Data](https://data.cdc.gov/Foodborne-Waterborne-and-Related-Diseases/NORS/5xkq-dg7x/about_data)
+
+### Outcome
+
+For the most part the function that uses threads is faster, I immagine that if I had more data to use it would show more of a difference.
+
+### What I want to work on in the future
+
+I want to add the other points of data to the output of the code.
--- parallel_task.py
+++ parallel_task.py
@@ -1,11 +1,9 @@
 import requests
-
 import multiprocessing as mp
 import threading
 import time
 from threading import Thread
 
-# The website I got the data from: https://data.cdc.gov/Foodborne-Waterborne-and-Related-Diseases/NORS/5xkq-dg7x/about_data
 TOP_API_URL = "https://data.cdc.gov/resource/5xkq-dg7x.json"
 
 class Request_thread(threading.Thread):
@@ -67,12 +65,17 @@ class Data_Info:
     
     def get_death(self):
         return self._death
+    
+comon_data_nop = {
+    "comon_month": 0,
+    "most_cases_year": 0,
+    "comon_year": 0,
+    "comon_state": None,
+    "most_cases_state": 0,
+    "total_deaths": 0
+    }
 
-# This function will keep track of the most comon month on incident.
-def comon_month(info, month_of_illness):
-    if info._month != None:
-        month_of_illness[int(info._month) - 1][1] += 1
-
+# This function interperets the API without using threads.
 def no_parallelism():
     request = Request_thread(f"{TOP_API_URL}")
     request.start()
@@ -80,9 +83,10 @@ def no_parallelism():
 
     disease_info = request.get_response()
 
-    total_deaths = 0
     month_of_illness = [[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0],[9,0],[10,0],[11,0],[12,0]]
-
+    year_of_illness = []
+    state_of_illness = [["Alabama",0], ["Alaska",0], ["Arizona",0], ["Arkansas",0], ["California",0], ["Colorado",0], ["Conneticut",0], ["Delaware",0], ["Florida",0], ["Georgia",0], ["Hawaii",0], ["Idaho",0], ["Illinois",0], ["Indiana",0], ["Iowa",0], ["Kansas",0], ["Kentucky",0], ["Louisiana",0], ["Maine",0], ["Maryland",0], ["Massachusetts",0], ["Michigan",0], ["Minnesota",0], ["Mississippi",0], ["Missouri",0], ["Montana",0], ["Nebraska",0], ["Nevada",0], ["New Hampshire",0], ["New Jersey",0], ["New Mexico",0], ["New York",0], ["North Carolina",0], ["North Dakota",0], ["Ohio",0], ["Oklahoma",0], ["Oregon",0], ["Pennsylvania",0], ["Rhode Island",0], ["South Carolina",0], ["South Dakota",0], ["Tennessee",0], ["Texas",0], ["Utah",0], ["Vermont",0], ["Virginia",0], ["Washington",0], ["West Virginia",0], ["Wisconsin",0], ["Wyoming",0]]
+    
     # If there is no data found then the program will tell the user so.
     if disease_info is None:
         print("No information found.")
@@ -90,37 +94,95 @@ def no_parallelism():
     
     for data_points in disease_info:
         info = Data_Info(data_points)
-        comon_month(info, month_of_illness)
+
+         # This statement will update the number of outbreaks that each month has.
+        if info._month is not None:
+            month_of_illness[int(info._month) - 1][1] += 1
+
+        # This statement will update the number of outbreaks in each year.
+        if info._year is not None:
+            if len(year_of_illness) <= 0:
+                year_of_illness.append([int(info._year), 1])
+            elif year_of_illness[-1][0] == int(info._year):
+                year_of_illness[-1][1] += 1
+            else:
+                year_of_illness.append([int(info._year), 1])
+        # print(type(year_of_illness[len(year_of_illness) - 1][0]))
+        # print(year_of_illness)
+
+        # This statement will update the number of outbreaks in each state.
+        if info._state is not None:
+            if len(state_of_illness) <= 0:
+                state_of_illness.append([info._state, 1])
+            else:
+                for state in state_of_illness:
+                    if state[0] == info._state:
+                        state[1] += 1
+        # print(state_of_illness)
+
 
         # Checks to see if the data in death is the string "Not Reported", if it isn't then the data is converted to an int and added to 
         # the total number of deaths.
         if info._death != "Not Reported":
-            total_deaths += int(info._death)
-        
-        print(info)
+            comon_data_nop["total_deaths"] += int(info._death)
+
+        # print(info)
     
     # Finds the month with the highest number of infections
-    most_comon_month = 0
     for set_of_data in month_of_illness:
-        if month_of_illness[most_comon_month - 1][1] < set_of_data[1]:
-            most_comon_month = set_of_data[0]
+        if month_of_illness[comon_data_nop["comon_month"] - 1][1] < set_of_data[1]:
+            comon_data_nop["comon_month"] = set_of_data[0]
+    
+    # Finds the year with the highest number of cases
+    for sets_of_data in year_of_illness:
+        year, cases = sets_of_data
+        if cases > comon_data_nop["most_cases_year"]:
+            comon_data_nop["most_cases_year"] = cases
+            comon_data_nop["comon_year"] = year
 
-    print(f"Most comon month for infection: {most_comon_month}")
-    print(f"Total deaths: {total_deaths}")
+    # Finds the state with the highest number of cases
+    for state_cases in state_of_illness:
+        state_occur, cases = state_cases
+        if cases > comon_data_nop["most_cases_state"]:
+            comon_data_nop["most_cases_state"] = cases
+            comon_data_nop["comon_state"] = state_occur
 
 #/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
-def parallel_function(data_lock, data_points, month_of_illness, total_deaths):
-    info = Data_Info(data_points)
-    comon_month(info, month_of_illness)
+comon_data = {
+    "comon_month": 0,
+    "most_cases_year": 0,
+    "comon_year": 0,
+    "comon_state": None,
+    "most_cases_state": 0,
+    "total_deaths": 0
+}
 
-    # Checks to see if the data in death is the string "Not Reported", if it isn't then the data is converted to an int and added to 
-    # the total number of deaths. The lock here is used to make sure that the data doesn't get mixed up with another thread.
-    if info._death != "Not Reported":
-        with data_lock:
-            total_deaths += int(info._death)
+def month_outbreak(month_of_illness, comon_data, lock):
+    # Finds the month with the highest number of infection.
+    with lock:
+        for set_of_data in month_of_illness:
+            if month_of_illness[comon_data["comon_month"] - 1][1] < set_of_data[1]:
+                comon_data["comon_month"] = set_of_data[0]
 
-    print(info)
+def year_outbreak(year_of_illness, comon_data, lock):
+    # Finds the year with the highest number of cases.
+    with lock:
+        for sets_of_data in year_of_illness:
+            year, cases = sets_of_data
+            if cases > comon_data["most_cases_year"]:
+                comon_data["most_cases_year"] = cases
+                comon_data["comon_year"] = year
+
+def state_outbreak(state_of_illness, comon_data, lock):
+    # Finds the state with the highest number of cases.
+    with lock:
+        for state_cases in state_of_illness:
+            state_occur, cases = state_cases
+            if cases > comon_data["most_cases_state"]:
+                comon_data["most_cases_state"] = cases
+                comon_data["comon_state"] = state_occur
+
 
 # The program with parallelism
 def with_parallelism():
@@ -130,50 +192,116 @@ def with_parallelism():
 
     disease_info = request.get_response()
 
-    total_deaths = 0
+    year_of_illness = []
     month_of_illness = [[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0],[9,0],[10,0],[11,0],[12,0]]
-
+    state_of_illness = [["Alabama",0], ["Alaska",0], ["Arizona",0], ["Arkansas",0], ["California",0], ["Colorado",0], ["Conneticut",0], ["Delaware",0], ["Florida",0], ["Georgia",0], ["Hawaii",0], ["Idaho",0], ["Illinois",0], ["Indiana",0], ["Iowa",0], ["Kansas",0], ["Kentucky",0], ["Louisiana",0], ["Maine",0], ["Maryland",0], ["Massachusetts",0], ["Michigan",0], ["Minnesota",0], ["Mississippi",0], ["Missouri",0], ["Montana",0], ["Nebraska",0], ["Nevada",0], ["New Hampshire",0], ["New Jersey",0], ["New Mexico",0], ["New York",0], ["North Carolina",0], ["North Dakota",0], ["Ohio",0], ["Oklahoma",0], ["Oregon",0], ["Pennsylvania",0], ["Rhode Island",0], ["South Carolina",0], ["South Dakota",0], ["Tennessee",0], ["Texas",0], ["Utah",0], ["Vermont",0], ["Virginia",0], ["Washington",0], ["West Virginia",0], ["Wisconsin",0], ["Wyoming",0]]
+    
     # If there is no data found then the program will tell the user so.
     if disease_info is None:
         print("No information found.")
         return
     
-    with parallel_semaphore:
-        for data_points in disease_info:
-            data_lock = threading.Lock()
-            data_thread = Thread(target=parallel_function, args=(data_lock, data_points, month_of_illness, total_deaths))
-            # thread_list.append(data_thread)
-            data_thread.start()
+    for data_points in disease_info:
+        info = Data_Info(data_points)
 
-    
-    # Finds the month with the highest number of infections
-    most_comon_month = 0
-    for set_of_data in month_of_illness:
-        if month_of_illness[most_comon_month - 1][1] < set_of_data[1]:
-            most_comon_month = set_of_data[0]
+        # This statement will update the number of outbreaks that each month has.
+        if info._month is not None:
+            month_of_illness[int(info._month) - 1][1] += 1
 
-    print(f"Most comon month for infection: {most_comon_month}")
-    print(f"Total deaths: {total_deaths}")
+        # This statement will update the number of outbreaks in each year.
+        if info._year is not None:
+            if len(year_of_illness) <= 0:
+                year_of_illness.append([int(info._year), 1])
+            elif year_of_illness[-1][0] == int(info._year):
+                year_of_illness[-1][1] += 1
+            else:
+                year_of_illness.append([int(info._year), 1])
 
-# thread_list = []
-MAX_SIZE = 10
-parallel_semaphore = threading.Semaphore(MAX_SIZE)
+        # This statement will update the number of outbreaks in each state.
+        if info._state is not None:
+            if len(state_of_illness) <= 0:
+                state_of_illness.append([info._state, 1])
+            else:
+                for state in state_of_illness:
+                    if state[0] == info._state:
+                        state[1] += 1
 
+        # Checks to see if the data in death is the string "Not Reported", if it isn't then the data is converted to an int and added to 
+        # the total number of deaths.
+        if info._death != "Not Reported":
+            comon_data["total_deaths"] += int(info._death)
+
+
+    lock = threading.Lock()
+    month_thread = threading.Thread(target=month_outbreak, args=(month_of_illness, comon_data, lock))
+    year_thread = threading.Thread(target=year_outbreak, args=(year_of_illness, comon_data, lock))
+    state_thread = threading.Thread(target=state_outbreak, args=(state_of_illness, comon_data, lock))
+
+    #month
+    month_thread.start()
+
+    #year
+    year_thread.start()
+
+    #state
+    state_thread.start()
+
+    month_thread.join()
+    year_thread.join()
+    state_thread.join()
+
+
+print("-----Without Parallelism-----")
 start_time_nop = time.time()
 no_parallelism()
 stop_time_nop = time.time()
+print(f"Most comon year for outbreak is {comon_data_nop['comon_year']} with {comon_data_nop['most_cases_year']} cases.")
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+print(f"Most common month for outbreak is {months[comon_data_nop['comon_month'] - 1]}")
+print(f"Most comon state for outbreak is {comon_data_nop['comon_state']} with {comon_data_nop['most_cases_state']}")
+print(f"Total deaths: {comon_data_nop['total_deaths']}")
+print(f"Time taken: {(stop_time_nop - start_time_nop):.2f} seconds")
 
-time.sleep(1)
+print()
 
+print("-----With Parallelism-----")
 start_time_withp = time.time()
 with_parallelism()
-# for threads in thread_list:
-#     threads.join()
 stop_time_withp = time.time()
+print(f"Most comon year for outbreak is {comon_data['comon_year']} with {comon_data['most_cases_year']} cases.")
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+print(f"Most common month for outbreak is {months[comon_data['comon_month'] - 1]}")
+print(f"Most comon state for outbreak is {comon_data['comon_state']} with {comon_data['most_cases_state']}")
+print(f"Total deaths: {comon_data['total_deaths']}")
+print(f"Time taken: {(stop_time_withp - start_time_withp):.2f} seconds")
 
-time.sleep(1)
+print()
 
-print(f"Time without parallelism: {(stop_time_nop - start_time_nop):.2f} seconds")
+print("-----Average Times From Twenty Runs-----")
+# Finding the average time for not using threads
+times_with_no_threads = []
+time_to_be_averaged_nop = 0
+for i in range(20):
+    start_time_nop = time.time()
+    with_parallelism()
+    stop_time_nop = time.time()
+    times_with_no_threads.append(stop_time_nop - start_time_nop)
 
-print(f"Time with parallelism: {(stop_time_withp - start_time_withp):.2f} seconds")
-# print(f"Number of threads: {len(thread_list)}")
+for times in times_with_no_threads:
+    time_to_be_averaged_nop += times
+
+print(f"Average time for no threads: {(time_to_be_averaged_nop / len(times_with_no_threads)):.2f} seconds")
+
+# Finding the average time using threads
+times_with_threads = []
+time_to_be_averaged_withp = 0
+for i in range(20):
+    start_time_withp = time.time()
+    with_parallelism()
+    stop_time_withp = time.time()
+    times_with_threads.append(stop_time_withp - start_time_withp)
+
+for times in times_with_threads:
+    time_to_be_averaged_withp += times
+
+print(f"Average time with using threads: {(time_to_be_averaged_withp / len(times_with_threads)):.2f} seconds")
